b0383b9454184f2b14f7276f696f62b9f6108a8f
Maharshi <007maharshigohel@gmail.com>
2024-07-02 11:41:36 -0400

    added commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Container, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [expenses, setexpenses] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/expenses").then((data) => {
+      console.log(data);
+      setexpenses(data.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Typography variant="h3">Expenses</Typography>
+        <ul>
+          {expenses.map((expense) => {
+            return <li key={expense.id}>{expense.title}    &nbsp;&nbsp;&nbsp; $ {expense.cost}</li>;
+          })}
+        </ul>
+      </Container>
     </div>
   );
 }
